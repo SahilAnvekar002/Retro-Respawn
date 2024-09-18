c552a6d90aa12310e494a93a8e8565690e7d5092
Sahil <sanvekar108@gmail.com>
2024-09-18 21:03:35 +0530

    final push

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           refs.current.forEach((ref) => {
             if (ref && isInViewport(ref)) {
               ref.classList.remove('opacity-0');
-              ref.classList.remove('translate-y-10');
+              ref.classList.remove('translate-y-20');
               ref.classList.add('opacity-100');
             }
           });
@@ -39,7 +39,7 @@ export default function Home() {
       refs.current.forEach((ref) => {
         if (ref && isInViewport(ref)) {
           ref.classList.remove('opacity-0');
-          ref.classList.remove('translate-y-10');
+          ref.classList.remove('translate-y-20');
           ref.classList.add('opacity-100');
         }
       })
@@ -63,8 +63,8 @@ export default function Home() {
 
   return (
     <>
-      <section className="flex flex-col bg-fixed bg-cover h-[100vh] bg-home-bg lg:bg-left bg-right">
-        <div className="flex flex-col md:px-24 xl:px-36 2xl:px-48 pt-56 transform translate-y-10 transition-[opacity,transform] duration-500 delay-300 opacity-0 md:items-start items-center" ref={(el) => { refs.current[0] = el }}>
+      <section className="flex flex-col lg:bg-fixed bg-cover h-[100vh] bg-home-bg lg:bg-left bg-right">
+        <div className="flex flex-col md:px-24 xl:px-36 2xl:px-48 pt-56 transform translate-y-20 transition-[opacity,transform] duration-500 delay-300 opacity-0 md:items-start items-center" ref={(el) => { refs.current[0] = el }}>
           <h1 className="text-white text-5xl sm:text-6xl font-bold tracking-widest italic mb-6">TEAM PLAY<br />AND SOLO<br />CONQUESTS</h1>
           <span className="text-white font-semiold tracking-widest mb-10 text-lg ">Our gaming adventure awaits!</span>
           <button className="px-8 py-3 bg-white text-black w-72 rounded-lg text-xl font-semibold hover:bg-transparent hover:text-white  hover:border-white border-2 transition-colors duration-300 ease-in-out">Learn More</button>
@@ -73,7 +73,7 @@ export default function Home() {
 
       <section className="py-14 sm:py-28" style={{ backgroundColor: 'rgb(246, 244, 255)' }}>
         <div className="flex justify-center">
-          <div className="flex flex-col items-center w-full px-4 sm:px-10 md:px-0 md:w-[700px] justify-center transform translate-y-10 transition-[opacity,transform] duration-500 delay-500 opacity-0" ref={(el) => { refs.current[1] = el }}>
+          <div className="flex flex-col items-center w-full px-4 sm:px-10 md:px-0 md:w-[700px] justify-center transform translate-y-20 transition-[opacity,transform] duration-500 delay-500 opacity-0" ref={(el) => { refs.current[1] = el }}>
             <span className="text-lg font-bold text-blue-800 mb-6">RETRO RESPAWN</span>
             <h1 className="font-bold sm:text-4xl text-3xl text-center mb-4">Expanding beyond gaming to grow sports and technology</h1>
             <p className="text-lg text-slate-700 text-center">We exist to champion its growth, as well as to explore and create investment opportunities in sports and financial technology.</p>
@@ -81,14 +81,14 @@ export default function Home() {
         </div>
 
         <div className="flex lg:flex-row flex-col pt-36 sm:pt-52 lg:px-24 md:px-12 sm:pb-20 pb-10">
-          <div className="w-full lg:w-1/2 px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-500 delay-300 opacity-0" ref={(el) => { refs.current[2] = el }}>
+          <div className="w-full lg:w-1/2 px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-500 delay-300 opacity-0" ref={(el) => { refs.current[2] = el }}>
             <span className="text-lg font-bold text-blue-700">WHAT WE DO</span>
             <h1 className="font-bold text-3xl sm:text-4xl mt-5">We’re based in India</h1>
             <p className="text-lg text-slate-700 my-5">Our head office is located in Kolhapur, India. From here, we manage the support and services we offer across India.</p>
             <a href="#" className="text-blue-600 font-semibold text-lg flex items-center">Read more <IconArrowRight className="ml-1" style={{ marginTop: '2px' }} /></a>
           </div>
 
-          <div className="lg:mt-0 mt-12 flex flex-col items-end w-full lg:w-1/2 px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-700 delay-1000 opacity-0" ref={(el) => { refs.current[3] = el }}>
+          <div className="lg:mt-0 mt-12 flex flex-col items-end w-full lg:w-1/2 px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-700 delay-1000 opacity-0" ref={(el) => { refs.current[3] = el }}>
             <div className="lg:max-h-[400px] w-full">
               <img src="https://images.unsplash.com/photo-1511512578047-dfb367046420?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="rounded-2xl w-full lg:h-full sm:h-[500px] shadow-md object-cover" />
             </div>
@@ -102,12 +102,12 @@ export default function Home() {
 
       <section className="py-12 sm:py-24">
         <div className="flex flex-col lg:px-24 md:px-12">
-          <div className="px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-500 delay-500 opacity-0" ref={(el) => { refs.current[4] = el }}>
+          <div className="px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-500 delay-500 opacity-0" ref={(el) => { refs.current[4] = el }}>
             <span className="text-lg font-bold text-blue-700">OUR CORE BRAND</span>
             <h1 className="font-bold text-3xl sm:text-4xl mt-5">Latest and best Games </h1>
             <p className="text-lg text-slate-700 my-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non hic tempora dignissimos iure amet! Autem et at animi hic, aut illo sapiente voluptatum, repellat optio architecto provident sed recusandae ullam!</p>
           </div>
-          <div className="px-4 sm:px-10 mt-6 transform translate-y-10 transition-[opacity,transform] duration-500 delay-500 opacity-0" ref={(el) => { refs.current[5] = el }}>
+          <div className="px-4 sm:px-10 mt-6 transform translate-y-20 transition-[opacity,transform] duration-500 delay-500 opacity-0" ref={(el) => { refs.current[5] = el }}>
             <img src="https://images.unsplash.com/photo-1725273578052-14575f0daa01?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" className="object-cover rounded-xl max-h-[400px] xl:max-h-[600px] w-full" />
             <a href="#" className="text-blue-600 font-semibold text-lg flex items-center mt-6">Read more <IconArrowRight className="ml-1" style={{ marginTop: '2px' }} /></a>
           </div>
@@ -116,14 +116,14 @@ export default function Home() {
 
       <section className="py-12 sm:py-24" style={{ backgroundColor: 'rgb(246, 244, 255)' }}>
         <div className="flex xl:flex-row flex-col lg:px-24 md:px-12">
-          <div className="flex flex-col self-start w-full lg:w-2/3 xl:w-1/2 px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-500 delay-300 opacity-0 xl:sticky top-28 xl:mb-0 mb-6" ref={(el) => { refs.current[6] = el }}>
+          <div className="flex flex-col self-start w-full lg:w-2/3 xl:w-1/2 px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-500 delay-300 opacity-0 xl:sticky top-28 xl:mb-0 mb-6" ref={(el) => { refs.current[6] = el }}>
             <span className="text-lg font-bold text-blue-700">OUR VALUES</span>
             <h1 className="font-bold text-3xl sm:text-4xl mt-5">What we stand for</h1>
             <p className="text-lg text-slate-700 my-5">We play an active role in pushing for a more transparent industry. We believe that a level playing field ensures that the ultimate beneficiaries are those players and communities we exist to serve.</p>
             <a href="#" className="text-blue-600 font-semibold text-lg flex items-center mt-6">About us<IconArrowRight className="ml-1" style={{ marginTop: '2px' }} /></a>
           </div>
 
-          <div className="flex flex-col w-full xl:w-1/2 px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-700 delay-1000 opacity-0" ref={(el) => { refs.current[7] = el }}>
+          <div className="flex flex-col w-full xl:w-1/2 px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-700 delay-1000 opacity-0" ref={(el) => { refs.current[7] = el }}>
             <div className="flex sm:flex-row flex-col shadow-md my-3 bg-white rounded-xl">
               <div className="flex justify-center items-center ml-5 mt-8 rounded-md h-14 w-[60px] xl:w-[20%]" style={{ backgroundColor: 'rgba(0,0,0,0.03)' }}>
                 <IconRosetteDiscountCheck fill="rgb(93, 95, 239)" color="white" size={44} />
@@ -166,7 +166,7 @@ export default function Home() {
 
       <section className="py-12 sm:py-24">
         <div className="flex xl:flex-row flex-col-reverse xl:items-center justify-center lg:px-24 md:px-12">
-          <div className="xl:block md:flex w-full xl:w-1/2 px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-500 delay-500 opacity-0 overflow-x-hidden" ref={(el) => { refs.current[8] = el }}>
+          <div className="xl:block md:flex w-full xl:w-1/2 px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-500 delay-500 md:delay-300 opacity-0 overflow-x-hidden" ref={(el) => { refs.current[8] = el }}>
             <div className="flex mb-4">
               <img src="https://images.pexels.com/photos/7862655/pexels-photo-7862655.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="max-h-[250px] object-cover rounded-xl xl:w-[48%] mr-4 md:min-w-[240px] w-[48%]" />
               <img src="https://images.pexels.com/photos/7862661/pexels-photo-7862661.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="max-h-[250px] object-cover rounded-xl xl:w-[48%] xl:mr-0 mr-4 md:min-w-[240px] w-[48%]" />
@@ -177,7 +177,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="w-full lg:w-2/3 xl:w-1/2 px-4 sm:px-10 transform translate-y-10 transition-[opacity,transform] duration-500 delay-500 opacity-0 xl:mb-0 sm:mb-12 mb-6" ref={(el) => { refs.current[9] = el }}>
+          <div className="w-full lg:w-2/3 xl:w-1/2 px-4 sm:px-10 transform translate-y-20 transition-[opacity,transform] duration-700 delay-500 md:delay-1000 opacity-0 xl:mb-0 sm:mb-12 mb-6" ref={(el) => { refs.current[9] = el }}>
             <span className="text-lg font-bold text-blue-700">E-SPORTS DEVELOPMENT</span>
             <h1 className="font-bold text-3xl sm:text-4xl mt-5">Investing in e-sports</h1>
             <p className="text-lg text-slate-700 my-5">To support the development and impact of esports, We re-invests revenue from betting. From partnering with national federations, to sponsoring talented individuals - we seek to generate value across the sports ecosystem. </p>
@@ -247,7 +247,7 @@ export default function Home() {
           <div className="flex flex-col justify-center items-center md:w-[750px] xl:w-[850px] sm:px-10 px-6 md:px-0">
             <h1 className="font-bold text-xl sm:text-2xl">Get in touch</h1>
             <p className="text-lg text-slate-700 mt-2 mb-4 text-center">If you’re interested in becoming a e-sports player, would like to discuss partnership opportunities or have media queries, we’d love to hear from you.</p>
-            <button className="px-6 py-2 bg-blue-700 text-white rounded-lg text-lg font-semibold hover:bg-blue-800 transition-colors duration-300 ease-in-out">Contact Us</button>
+            <button className="px-4 sm:px-6 py-1 sm:py-2 bg-blue-700 text-white rounded-lg text-lg font-semibold hover:bg-blue-800 transition-colors duration-300 ease-in-out">Contact Us</button>
             <div className="flex mt-8">
               <div className="rounded-full w-10 h-10 flex items-center justify-center mr-3 hover:brightness-75 transition-all duration-300 ease-out cursor-pointer" style={{ backgroundColor: 'rgb(37, 42, 45)' }}>
                 <IconBrandLinkedin size={30} color="rgb(37, 42, 45)" fill="white" />
